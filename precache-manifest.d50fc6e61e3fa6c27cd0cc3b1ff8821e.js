9455674f740b1a77c1452e8a55a6a5566715ca18
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2020-02-12 09:33:34 +0530

    Updates

---
+++ precache-manifest.d50fc6e61e3fa6c27cd0cc3b1ff8821e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87af19ae093885fb0d883ff8cd46c25d",
+    "url": "/stc-website/index.html"
+  },
+  {
+    "revision": "89f6397df276ff8a3cca",
+    "url": "/stc-website/static/css/main.c2fd76b1.chunk.css"
+  },
+  {
+    "revision": "ef9d98b8823489666fe7",
+    "url": "/stc-website/static/js/2.c0dfe9b5.chunk.js"
+  },
+  {
+    "revision": "94a3d75db89699ee2ef9f4f4d471747b",
+    "url": "/stc-website/static/js/2.c0dfe9b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f6397df276ff8a3cca",
+    "url": "/stc-website/static/js/main.590d04db.chunk.js"
+  },
+  {
+    "revision": "86c26e00c3b661a7cbe6",
+    "url": "/stc-website/static/js/runtime-main.d9217c44.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/stc-website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/stc-website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/stc-website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/stc-website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/stc-website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/stc-website/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/stc-website/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/stc-website/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/stc-website/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/stc-website/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "8dd65439b6d2e85e55e9a756dfda0b3c",
+    "url": "/stc-website/static/media/promo-1.8dd65439.png"
+  }
+]);
