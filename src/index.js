3f6d6d8d08b61d09e7a0ffcbdd897f65f4167207
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2020-02-12 09:40:27 +0530

    React-router addition

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
 
 import "../src/assets/vendor/nucleo/css/nucleo.css";
 import "../src/assets/vendor/font-awesome/css/font-awesome.min.css";
 import "../src/assets/scss/argon-design-system-react.scss";
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path='/' exaxt render={props =><App {...props} />} />
+            <Redirect ro='/' />
+        </Switch>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    </BrowserRouter>
+    , document.getElementById('root'));
+
