8438d2a173290d72c97348803b4343b74cfdd1d1
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2020-02-13 13:56:34 +0530

    design changes

--- src/contact.component.jsx
+++ src/contact.component.jsx
@@ -33,21 +33,7 @@ import {
               <div className="shape shape-style-1 shape-dark">
               
             </div>
-            <div className="separator separator-bottom separator-skew zindex-100">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-              >
-                <polygon
-                  className="fill-white"
-                  points="2560 0 2560 100 0 100"
-                />
-              </svg>
-            </div>
+           
                 <section className="section section-lg pt-lg-0 section-contact-us">
                 
             <Container>
